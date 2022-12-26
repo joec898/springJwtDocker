9f7bad08f1d1a913002ca344c5101aa088b01ddf
joe c <joec898@gmail.com>
2022-12-26 12:51:32 -0600

    restored unused WebSecurityConfig

--- src/main/java/com/jctech/springbootJwt/security/SecurityConfig.java
+++ src/main/java/com/jctech/springbootJwt/security/SecurityConfig.java
@@ -4,30 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder; 
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.jctech.springbootJwt.security.jwt.AuthEntryPointJwt;
 import com.jctech.springbootJwt.security.jwt.AuthTokenFilter;
 import com.jctech.springbootJwt.security.services.UserDetailsServiceImpl;
 
-/**
- * This is not used 
- *
- */
-
-//@Configuration
-//@EnableGlobalMethodSecurity(
+@Configuration
+@EnableGlobalMethodSecurity(
 		// securedEnabled = true,
 		// jsr250Enabled = true,
-//		prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		prePostEnabled = true)
+public class SecurityConfig {  
+
 	@Autowired
 	UserDetailsServiceImpl userDetailsService;
 
@@ -39,15 +36,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new AuthTokenFilter();
 	}
 
-	@Override
-	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    // Injecting Google custom authentication provider
+    @Autowired
+    MockAuthenticationProvider googleCloudAuthenticationProvider;
+ 
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
 	}
 
 	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
 	}
 
 	@Bean
@@ -55,13 +59,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-				.antMatchers("/api/auth/**").permitAll().antMatchers("/api/test/**").permitAll().anyRequest()
-				.authenticated();
+				.antMatchers("/**").permitAll()
+				.antMatchers("/api/auth/**").permitAll()
+				.antMatchers("/api/test/**").permitAll().anyRequest().authenticated();
+		
+		http.authenticationProvider(authenticationProvider()).authenticationProvider(googleCloudAuthenticationProvider);
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
 	}
+
 }
+
+
